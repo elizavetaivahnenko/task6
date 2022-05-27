71293ca1ca511b73e20aae2ddb3074b4f8875d9b
ivakhelo_e <elizavetaivahnenko3@gmail.com>
2022-05-27 22:28:12 +0300

    finish

--- index.js
+++ index.js
@@ -1,110 +1,76 @@
 
-/*
-//что если я хочу скопировать один массив и вставить его в новый ка это сделать?
+// // task 1
+// function repeatMsg(countMaxMsg) {
+//     countMaxMsg = prompt('Hi, enter some value');
+//     let i;
+//     for (i = 1; i <= countMaxMsg; i++){
+//         console.log('*');
+//     }
+// }
 
-let task1 = ['liza', 'vasiliy', 'vadim'];
-let i;
-for (i = task1.length - 1; i >= 0;  i--){
-    console.log(task1[i]);
-}
-
-let task2 = [10, 20, 50, 100, 0, 0, 300, 0, 500, 600, 0, 0];
-let count=0;
-for (i = 0; i < task2.length; i++){
-    if (task2[i] == 0) {
-        console.log(i);
-        count += 1;
-    }
-}
-
-for (i = 0; i < task2.length; i++){
-    if (task2[i] == 0) break;
-    console.log(task2[i]);
-    count++
-}
-
-for (i; i < task2.length-1; i++){
-    count = task2[i] + task2[i + 1];
-    console.log(count);
-}
-
-let arr = []; j; 
-for (i = 0; i < task2.length; i++){
-    count[j] = a[j] + task2[i + 1];
-}
-
-for (i = 0; i < task2.length; i++){
-    if (task2[i] == 0) break;
-    console.log(task2[i]);
-}
-
-let arrZeroIndexes = [];
-
-for (i = 0; i < task2.length; i++){
-    if (task2[i] == 0) {
-        arrZeroIndexes[] = i;
-    }
-} 
-*/
-
-
-
-// task 1
-function repeatMsg(countMaxMsg) {
-    countMaxMsg = prompt('Hi, enter some value');
-    let i;
-    for (i = 1; i <= countMaxMsg; i++){
-        console.log('*');
-    }
-}
-
-//task2
-let arr = [50, 75, 2, 35, 55, 16, 100, 40, 97, 356, 899];
-lim1 = 10; lim2 = 300; i;
-function task2(arr){
-    for (i = lim1; i <= lim2; i++){
-        if (arr[i] % 5 == 0) {
-            console.log(arr[i]);
-        }
-    }
-}
+// //task2
+ let array = [50, 75, 2, 35, 55, 1, 100, 40, 97, 356, 899];
+// function task2(arr) {
+//     let arr2 = [], j=0, i;
+//     for (i = 0; i < arr.length; i++){
+//         if (arr[i] % 5 == 0) {
+//             // while (arr[i] < 10) {
+//             // }
+//             //не получается задать лимиты
+//              arr2[j] = arr[i];
+//              j++;
+//         }
+//     }
+//     return arr2;
+// }
+// let resultTask2;
+// resultTask2 = task2(array);
+// console.log(resultTask2);
 
 //task3
 
-let summEvenNumber = 0;
-function task3(arr) {
-    for (i = 0; i <= arr[i]; i++){
-        if (arr[i] % 2 == 0) {
-            summEvenNumber += i;
-            console.log(summEvenNumber);
-        }
-    }
-}
+// function task3(a) {
+//     let i , summEvenNumber=0;
+//     for (i = 0; i < a.length; i++){
+//         if (a[i] % 2 == 0) {
+//             summEvenNumber++;
+//         }
+//     }
+//     return summEvenNumber;
+// }
+// let resultTask3;
+// resultTask3 = task3(array);
+// console.log(resultTask3);
 
 
-//task4
-function task4(arr) {
-    for (i = 0; i <= arr[i]; i++) {
-        if (arr[i] <= 10) {
-            arr[i] == 0;
-        }
-    }
-}
 
-//task5
+// //task4
+// function task4(arr) {
+//     for (i = 0; i < arr.length; i++) {
+//         if (arr[i] <= 10) {
+//             arr[i] == 0;
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+// let resultTask4;
+// resultTask4 = task4(array);
+// console.log(resultTask4);
 
-let wholeNumbers = [-4, 6, 10, 5, 8, -3, -5.3]; i;
-let finishedResult = [];
-function num(wholeNumbers) {
-    if (wholeNumbers[i] < 0) {
-        wholeNumbers[i] *= 1000;
-        finishedResult[wholeNumbers];
-    }
-    else {
-        wholeNumbers[i] = i + ',0';
-        finishedResult[wholeNumbers];
-    }
-}
+// //task5
+
+// let wholeNumbers = [-4, 6, 10, 5, 8, -3, -5.3]; i;
+// let finishedResult = [];
+// function num(wholeNumbers) {
+//     if (wholeNumbers[i] < 0) {
+//         wholeNumbers[i] *= 1000;
+//         finishedResult[wholeNumbers];
+//     }
+//     else {
+//         wholeNumbers[i] = i + ',0';
+//         finishedResult[wholeNumbers];
+//     }
+// }
 
 
 
